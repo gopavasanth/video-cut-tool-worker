fc8fc5052f8ba8dbdec82a51fa118f7cfeda962e
gopavasanth <gopavasanth1999@gmail.com>
2021-04-17 16:25:51 +0530

    Udates of v2.0.0

--- config.js
+++ config.js
@@ -1,7 +1,21 @@
-const config = {
-    REDIS_HOST: 'video-cut-tool',
-    REDIS_PORT: 6379,
-    REDIS_PASSWORD: '', 
-};
+function config() {
+	switch (process.env.NODE_ENV) {
+		case 'production':
+			return {
+				REDIS_HOST: 'video-cut-tool',
+				REDIS_PORT: 6379,
+				REDIS_PASSWORD: '',
+				API_ROOT: 'https://videocuttool.wmflabs.org/video-cut-tool-back-end'
+			};
+
+		default:
+			return {
+				REDIS_HOST: '127.0.0.1',
+				REDIS_PORT: 6379,
+				REDIS_PASSWORD: '',
+				API_ROOT: 'http://localhost:4000'
+			};
+	}
+}
 
 module.exports = config;
